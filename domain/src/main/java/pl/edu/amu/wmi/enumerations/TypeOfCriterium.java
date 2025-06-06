160c0fd2c50247e03d1dd3b0bcdba98db4ec337e
Krzysztof Rożek <seriousemail468@gmail.com>
2025-06-06 21:01:52 +0200

    Fixed the intiger column error, POST succesfully works

--- domain/src/main/java/pl/edu/amu/wmi/enumerations/TypeOfCriterium.java
+++ domain/src/main/java/pl/edu/amu/wmi/enumerations/TypeOfCriterium.java
@@ -1,31 +1,7 @@
 package pl.edu.amu.wmi.enumerations;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum TypeOfCriterium
 {
     REQUIRED, EXPECTED, MEASURABLE_IMPLEMENTATION_INDICATORS;
 
-//    private final String label;
-//
-//    TypeOfCriterium(String label)
-//    {
-//        this.label=label;
-//    }
-//    @JsonValue
-//    public String getLabel() {
-//        return label;
-//    }
-//
-//
-//    @JsonCreator
-//    public static TypeOfCriterium fromLabel(String label) {
-//        for (TypeOfCriterium value : TypeOfCriterium.values()) {
-//            if (value.label.equalsIgnoreCase(label)) {
-//                return value;
-//            }
-//        }
-//        throw new IllegalArgumentException("Unknown type: " + label);
-//    }
 }
