7fa269adb6b993d0a89ece619592c21a49c653a5
Kasia Strzyzewska <kasia@example.com>
2025-06-13 22:58:10 +0200

    Revert

---
+++ criteria-project/src/main/java/pl/edu/amu/wmi/dto/UpdateCommentAndLevelDTO.java
@@ -0,0 +1,10 @@
+package pl.edu.amu.wmi.dto;
+
+import lombok.Data;
+import pl.edu.amu.wmi.enumerations.LevelOfRealization;
+
+@Data
+public class UpdateCommentAndLevelDTO {
+    private String comment;
+    private LevelOfRealization levelOfRealization;
+}
