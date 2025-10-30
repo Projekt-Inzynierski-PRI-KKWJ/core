775a828f64812e9dd57d7d60905822845dc579d3
Kasia Strzyzewska <kasia@example.com>
2025-10-30 10:23:53 +0100

    SCRUM-75 change response entity

--- project-market/src/main/java/pl/edu/amu/wmi/web/controller/ProjectDataController.java
+++ project-market/src/main/java/pl/edu/amu/wmi/web/controller/ProjectDataController.java
@@ -6,9 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.amu.wmi.web.ProjectMarketFacade;
 import pl.edu.amu.wmi.exception.BusinessException;
-import pl.edu.amu.wmi.web.mapper.ProjectMarketMapper;
 import pl.edu.amu.wmi.web.model.ProjectCreateRequestDTO;
-import pl.edu.amu.wmi.web.model.ProjectMarketDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,14 +14,13 @@ import pl.edu.amu.wmi.web.model.ProjectMarketDTO;
 public class ProjectDataController {
 
     private final ProjectMarketFacade projectMarketFacade;
-    private final ProjectMarketMapper projectMarketMapper;
 
     @PostMapping
-    public ResponseEntity<ProjectMarketDTO> createProjectAndPublishOnMarket(@Valid @RequestBody ProjectCreateRequestDTO request) {
+    public ResponseEntity<Void> createProjectAndPublishOnMarket(@Valid @RequestBody ProjectCreateRequestDTO request) {
         //UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         try {
-            var projectMarket = projectMarketFacade.createMarket(request, "s485953");
-            return ResponseEntity.ok(projectMarketMapper.toDto(projectMarket));
+            projectMarketFacade.createMarket(request, "s485953");
+            return ResponseEntity.ok().build();
         } catch (Exception e) {
             throw new BusinessException(e.getMessage());
         }
--- project-market/src/main/java/pl/edu/amu/wmi/web/mapper/ProjectMarketMapper.java
+++ project-market/src/main/java/pl/edu/amu/wmi/web/mapper/ProjectMarketMapper.java
@@ -6,18 +6,14 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 import pl.edu.amu.wmi.entity.Project;
-import pl.edu.amu.wmi.entity.ProjectMarket;
 import pl.edu.amu.wmi.model.PublishProjectMarketRequest;
 import pl.edu.amu.wmi.web.model.ProjectCreateRequestDTO;
-import pl.edu.amu.wmi.web.model.ProjectMarketDTO;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProjectMarketMapper {
 
-
-    ProjectMarketDTO toDto(ProjectMarket projectMarket);
 
     @Mapping(target = "project", source = "project")
     @Mapping(target = "maxMembers", source = "requestDTO.maxMembers")
