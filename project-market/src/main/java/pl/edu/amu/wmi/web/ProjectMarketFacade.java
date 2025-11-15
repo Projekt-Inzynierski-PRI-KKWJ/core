e449da26f1d7310380ae89ee68df514fef59794e
Kasia Strzyzewska <kasia@example.com>
2025-11-15 18:49:27 +0100

    Change market statuses

--- project-market/src/main/java/pl/edu/amu/wmi/web/ProjectMarketFacade.java
+++ project-market/src/main/java/pl/edu/amu/wmi/web/ProjectMarketFacade.java
@@ -12,6 +12,7 @@ import pl.edu.amu.wmi.dao.StudentDAO;
 import pl.edu.amu.wmi.dao.StudyYearDAO;
 import pl.edu.amu.wmi.entity.ProjectApplication;
 import pl.edu.amu.wmi.enumerations.ProjectApplicationStatus;
+import pl.edu.amu.wmi.enumerations.ProjectMarketStatus;
 import pl.edu.amu.wmi.enumerations.ProjectRole;
 import pl.edu.amu.wmi.service.ProjectApplicationService;
 import pl.edu.amu.wmi.service.ProjectMarketService;
@@ -77,6 +78,9 @@ public class ProjectMarketFacade {
         String indexNumber = "s485940";
         var student = studentDAO.findByUserData_IndexNumber(indexNumber);
         var projectMarket = projectMarketService.getByProjectMarketId(marketId);
+        if (ProjectMarketStatus.ACTIVE != projectMarket.getStatus()) {
+            throw new IllegalStateException("Project market is not active.");
+        }
         projectApplicationService.applyToMarket(applyToProjectRequestMapper.fromDTO(request, student, projectMarket));
     }
 
