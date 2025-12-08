8d4d2d598e0b0b81659b8b817b53d9270e738e39
Wiktor Gołąb <31176088+WiktorGolab@users.noreply.github.com>
2025-12-08 17:14:45 +0100

    fix

--- pri-application/src/main/java/pl/edu/amu/wmi/PriApplication.java
+++ pri-application/src/main/java/pl/edu/amu/wmi/PriApplication.java
@@ -2,8 +2,10 @@ package pl.edu.amu.wmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"pl.edu.amu.wmi", "pl.edu.amu.projectmarket"})
 public class PriApplication {
 
     public static void main(String[] args) {
